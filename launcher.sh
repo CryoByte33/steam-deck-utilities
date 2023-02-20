9be825543547a093eee4f3deb118a7175b4d85f2
Augustin Cisterne-Kaas <ajitekun@gmail.com>
2023-02-20 12:56:38 +0800

    fix: button did not display correctly and windows was resizing weirdly without scale at 0.25

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-if [ "$(xdpyinfo | grep dimension | awk '{print $2}' | cut -d 'x' -f2)" -eq "800" ]; then
-  export FYNE_SCALE=0.25
-fi
+export FYNE_SCALE=0.25
 
 "$HOME"/.cryo_utilities/cryo_utilities gui
