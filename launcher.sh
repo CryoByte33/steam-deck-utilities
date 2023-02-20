704ad7e9f376cfa581a8cac33277b3b381917ed6
Augustin Cisterne-Kaas <ajitekun@gmail.com>
2023-02-20 15:28:32 +0800

    fix: no need to use verbose

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,5 @@
 !/bin/bash
-if [ "$(xrandr --current --verbose | grep '*' | xargs | cut -d ' ' -f1)" = "800x1280" ]; then
+if [ "$(xrandr --current | grep '*' | xargs | cut -d ' ' -f1)" = "800x1280" ]; then
   export FYNE_SCALE=0.25
 fi
 
