2e3c4fafddaeebc1b61b2eebcd11f045adbeeb4e
Augustin Cisterne-Kaas <ajitekun@gmail.com>
2023-02-20 14:31:26 +0800

    fix: use xrandr instead of xdypinfo to detect steamdeck screen

--- launcher.sh
+++ launcher.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
-export FYNE_SCALE=0.25
+!/bin/bash
+if [ "$(xrandr --current --verbose | grep '*' | xargs | cut -d ' ' -f1)" = "800x1280" ]; then
+  export FYNE_SCALE=0.25
+fi
 
 "$HOME"/.cryo_utilities/cryo_utilities gui
