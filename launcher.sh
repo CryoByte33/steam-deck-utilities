b5c56f40ba6dd1672c9a63aa8242b04d017f5e48
skaluzka <skaluzka@protonmail.com>
2023-09-26 19:43:08 +0200

    Improve launcher.sh script
    Use grep with -c flag instead of wc -l command.
    Additionally, change quotation of path pointing to cryo_utilities
    binary.
    
    Signed-off-by: skaluzka <skaluzka@protonmail.com>

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-if [ "$(xrandr | grep ' connected' | wc -l)" -eq 1 ]; then
+# Author: CryoByte33 and contributors to the CryoUtilities project
+
+if [ "$(xrandr | grep -c ' connected')" -eq 1 ]; then
   export FYNE_SCALE=0.25
 fi
 
-"$HOME"/.cryo_utilities/cryo_utilities gui
+"$HOME/.cryo_utilities/cryo_utilities" gui
