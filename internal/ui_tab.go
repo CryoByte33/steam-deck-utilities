841f780ae7f7630e96fbc06680a380e917cb0c8d
Max Blazejewski <man90@tuta.io>
2023-11-29 08:28:20 +0100

    Add a button that reboots the device to bootloader (#137)
    * Dummy version change to create and test develop branch
    * Fix develop release flow
    * Add a button that reboots the device to bootloader
    
    ---------
    
    Co-authored-by: CryoByte33 <cryobyte33@gmail.com>

--- internal/ui_tab.go
+++ internal/ui_tab.go
@@ -287,13 +287,11 @@ func (app *Config) vramTab() *fyne.Container {
 	// Get VRAM value
 	app.refreshVRAMContent()
 
-	textHowTo := widget.NewLabel("1. Turn off the Steam Deck\n\n" +
-			"2. Press and hold the volume up button, press the power button, then release both\n\n" +
-			"3. Navigate to Setup Utility -> Advanced -> UMA Frame Buffer Size")
-	
+	textHowTo := widget.NewLabel("1. Press the button below to reboot to bootloader\n\n" +
+		"2. Navigate to Setup Utility -> Advanced -> UMA Frame Buffer Size")
+
 	textRecommended := widget.NewLabelWithStyle("4G is the recommended setting for most situations", fyne.TextAlignLeading, fyne.TextStyle{Bold: true})
 	textWarning := widget.NewLabel("Please be aware that some games (RDR2) may experience degraded performance.")
-
 
 	textVBox := container.NewVBox(
 		textHowTo,
@@ -302,6 +300,12 @@ func (app *Config) vramTab() *fyne.Container {
 	)
 
 	vramCard := widget.NewCard("Minimum VRAM", "How to change the minimum VRAM:", textVBox)
+
+	rebootToBootloaderButton := widget.NewButton("Reboot to bootloader", func() {
+		if err := rebootToBootloader(); err != nil {
+			rebootFailedWindow()
+		}
+	})
 
 	vramBAR := container.NewGridWithColumns(1,
 		container.NewCenter(app.VRAMText))
@@ -313,6 +317,7 @@ func (app *Config) vramTab() *fyne.Container {
 
 	vramVBOX := container.NewVBox(
 		vramCard,
+		rebootToBootloaderButton,
 	)
 	scroll := container.NewScroll(vramVBOX)
 	full := container.NewBorder(topBar, nil, nil, nil, scroll)
